c7fbdc94516a22e1568dfc8f8fdab39b6f30072c
gabrieljorda <gabrieleminhamae@hotmail.com>
2025-12-22 13:25:23 -0300

    adicionando arquivos iniciais de aprendizado em Python

---
+++ 1-priemeiro.py
@@ -0,0 +1,11 @@
+#aprendendo a Linguagem python 
+print("Olá mundo")
+print("Aprendendo a linguagem Python")
+"""
+Aprendendo Python 
+"""
+#exerciio
+#Você deve ser capaz de criar um programa que exiba a 
+#mensagem Hello World no terminal.
+
+print("hello world")
---
+++ 2-dados.py
@@ -0,0 +1,24 @@
+ #Pythonflix
+name = "Top Gun Maverick"
+yearLaunch = 2023
+notemovie = 9.5
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(notemovie))
+print(type(planIncluded))
+#Escreva um programa que imprima na tela, 
+# cada um em uma linha, um número inteiro, 
+# um número decimal (float) e um texto (string).
+
+inteiro = 1 
+descimal = 1.1 
+palavra = "olá Mundo"
+
+print(inteiro, "\n",type(inteiro))
+print(descimal, "\n",type(descimal))
+print(palavra , "\n",type(palavra))
