f1477c947867d727d9df9c666970ad3aad50260d
gabrieljorda <gabrieleminhamae@hotmail.com>
2025-12-22 17:17:03 -0300

    .

---
+++ 3-input.py
@@ -0,0 +1,8 @@
+#utilizando input 
+nome = input("Digite o nome do filme:\n")
+yearLaunch = int(input("Digite o ano do Lançamento:\n"))
+noteMovie = float(input("Digite a:\n"))
+
+print(nome)
+print(yearLaunch)
+print(noteMovie)
